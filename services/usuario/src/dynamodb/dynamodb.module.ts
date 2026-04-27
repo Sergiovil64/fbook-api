be83d5743a9cfb4d30140a23cd57332ada4b0082
Sergio Villafan <villafan815@gmail.com>
2026-04-26 21:59:42 -0400

    Solucionar issue secrets

--- services/usuario/src/dynamodb/dynamodb.module.ts
+++ services/usuario/src/dynamodb/dynamodb.module.ts
@@ -10,13 +10,13 @@ import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
       useFactory: () => {
         const client = new DynamoDBClient({
           region: process.env.AWS_REGION ?? 'us-east-1',
-          ...(process.env.DYNAMODB_ENDPOINT && { endpoint: process.env.DYNAMODB_ENDPOINT }),
-          ...(process.env.AWS_ACCESS_KEY_ID && {
+          ...(process.env.DYNAMODB_ENDPOINT ? { endpoint: process.env.DYNAMODB_ENDPOINT } : {}),
+          ...(process.env.AWS_ACCESS_KEY_ID ? {
             credentials: {
               accessKeyId: process.env.AWS_ACCESS_KEY_ID,
               secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
             },
-          }),
+          } : {}),
         });
         return DynamoDBDocumentClient.from(client);
       },
